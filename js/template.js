c30952e93e9e9c19d0c4a57aabddc6b61f80a417
hamitmizrak <hamitmizrak@gmail.com>
2023-01-31 19:27:57 +0300

    for , if ,

--- js/template.js
+++ js/template.js
@@ -87,37 +87,100 @@
 // twoNumberSum7();
 
 
-let twoNumberSum9 = () => {
-    let number1;
-    number1 = Number(prompt("Lütfen sayı giriniz."));
-    let data = (number1 < 0) ? "negatif" : "pozitif";
-    console.log(data)
+// let twoNumberSum9 = () => {
+//     let number1;
+//     number1 = Number(prompt("Lütfen sayı giriniz."));
+//     let data = (number1 < 0) ? "negatif" : "pozitif";
+//     console.log(data)
+// }
+// //twoNumberSum9();
+
+
+// let twoNumberSum12 = () => {
+//     console.log((Number(prompt("Lütfen sayı giriniz.")) < 0) ? "negatif" : "pozitif")
+// }
+// //twoNumberSum12();
+
+
+// let twoNumberSum7 = () => {
+//     let number1, number2;
+//     number1 = Number(prompt("Lütfen sayı giriniz."));
+
+//     if (number1 === 1) {
+//         console.log("1");
+//     } else if (number1 === 2) {
+//         console.log("2");
+//     } else if (number1 === 3) {
+//         console.log("3");
+//     } else if (number1 === 4) {
+//         console.log("4");
+//     } else if (number1 === 5) {
+//         console.log("5");
+//     } else {
+//         console.log("1<=X<=5 dışındadır");
+//     }
+// }
+// //twoNumberSum7();
+
+//LOOP
+//iterative
+let data12 = () => {
+    let number = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+    document.write(number + "<br/>")
 }
-//twoNumberSum9();
+//data12()
 
 
-let twoNumberSum12 = () => {
-    console.log((Number(prompt("Lütfen sayı giriniz.")) < 0) ? "negatif" : "pozitif")
+//for loop
+let data13 = () => {
+    let sum = 0;
+    for (let i = 1; i <= 10; i++) {
+        //sum = sum + i;
+        sum += i;
+    }
+    document.write(sum + "<br/>");
 }
-twoNumberSum12();
+//data13()
 
 
-let twoNumberSum7 = () => {
-    let number1, number2;
-    number1 = Number(prompt("Lütfen sayı giriniz."));
+//while loop
+let data14 = () => {
+    let sum = 0;
+    let i = 1;
+    while (i <= 10) {
+        sum += i;
+        i++;
+    }
+    document.write(sum + "<br/>");
+}
+//data14()
 
-    if (number1 === 1) {
-        console.log("1");
-    } else if (number1 === 2) {
-        console.log("2");
-    } else if (number1 === 3) {
-        console.log("3");
-    } else if (number1 === 4) {
-        console.log("4");
-    } else if (number1 === 5) {
-        console.log("5");
-    } else {
-        console.log("1<=X<=5 dışındadır");
+//do-while loop
+let data15 = () => {
+    let sum = 0;
+    let i = 1;
+    do {
+        sum += i;
+        i++;
+    } while (i <= 10);
+    document.write(sum + "<br/>");
+}
+//data15();
+
+
+//1-10 arasındaki 2'ile bölünebilen sayıların toplamı veren algoritması ?
+//for - if
+let data16 = () => {
+    let evenSum = 0, oddSum = 0;
+    for (let i = 1; i <= 10; i++) {
+        //çift
+        if (i % 2 == 0) {
+            evenSum += i;
+        } else {
+            oddSum += i;
+        }
+        console.log("çift: " + evenSum)
+        console.log("tek: " + oddSum)
     }
 }
-//twoNumberSum7();
+//data16()
